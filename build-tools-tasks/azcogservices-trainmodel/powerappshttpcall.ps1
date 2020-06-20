35ea971b4fa3108fa8fa6ea9c5f2263b2f0b6071
Jack Murphy <jmurphy@hsdyn.com>
2020-06-20 15:51:09 +0100

    update

--- build-tools-tasks/azcogservices-trainmodel/powerappshttpcall.ps1
+++ build-tools-tasks/azcogservices-trainmodel/powerappshttpcall.ps1
@@ -7,7 +7,7 @@ $ErrorActionPreference = "Stop"
 Write-Verbose 'Entering powerappshttpcall.ps1'
 
 #Get Parameters
-$httpEndPoint = Get-VstsInput -Name httpEndPoint -Require
+$httpEndPoint = -Name httpEndPoint -Require
 
 Invoke-WebRequest -Uri "$httpEndPoint" -Method POST
 
