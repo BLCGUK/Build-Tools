6f68827992abcecbcdeef4cd9ac175780cb2fa40
Jack Murphy <jmurphy@hsdyn.com>
2020-06-20 17:50:26 +0100

    commit

--- build-tools-tasks/azcustomvision-trainmodel/azcustomvisiontrainmodel.ps1
+++ build-tools-tasks/azcustomvision-trainmodel/azcustomvisiontrainmodel.ps1
@@ -11,4 +11,7 @@ $httpEndPoint = -Name httpEndPoint -Require
 
 Invoke-WebRequest -Uri "$httpEndPoint" -Method POST
 
+Invoke-WebRequest -UseBasicParsing http://example.com/service -ContentType "application/json" -Training-Key "$httpEndPoint" -Method POST -Body "{ 'ItemID':3661515, 'Name':'test'}"
+
+
 Write-Verbose 'Leaving powerappshttpcall.ps1'
