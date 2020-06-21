dfc75bbdfa1381e619d375e1a8660decf830695d
Jack Murphy <jmurphy@hsdyn.com>
2020-06-21 14:37:32 +0100

    update

--- build-tools-tasks/azcustomvision-trainmodel/azcustomvisiontrainmodel.ps1
+++ build-tools-tasks/azcustomvision-trainmodel/azcustomvisiontrainmodel.ps1
@@ -9,6 +9,15 @@ Write-Verbose 'Entering powerappshttpcall.ps1'
 #Get Parameters
 $customVisionEndPoint = -Name Get-VstsInput -Name cognitiveServicesEndPoint -Require
 
+if (1 -eq 1) {
+    'This number is  1'
+  
+    }  Else {
+  
+    'This number is  not 1'
+  
+  } 
+
 Invoke-WebRequest -Uri "$customVisionEndPoint" -Method POST
 
 Write-Verbose 'Leaving powerappshttpcall.ps1'
